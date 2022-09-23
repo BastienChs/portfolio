35e1a127ea541a052b8ff75f98df8c32d6271ce1
BastienChs <bastien.chies@gmail.com>
2022-09-23 17:19:45 +0200

    Fixing tailwind problem.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
     "./src/pages/**/*.{js,ts,jsx,tsx}",
     "./src/components/**/*.{js,ts,jsx,tsx}",
-
+    "./src/Components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
