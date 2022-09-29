3fae1f802f961f607219d857146d69cd7436e49b
BastienChs <bastien.chies@gmail.com>
2022-09-29 11:51:30 +0200

    Fix Experiences section responsive issues.

--- components/Experiences.jsx
+++ components/Experiences.jsx
@@ -15,15 +15,15 @@ export const Experiences = ({experiences, locale}) => {
                                 <div className="space-y-12 my-16" key={experience.Id}>
                                     <div
                                         className="w-full border border-nav p-4 lg:px-32 lg:py-20 lg:space-x-32 flex justify-center lg:justify-start flex-wrap lg:flex-nowrap">
-                                        <div className="mb-6 lg:mb-0  w-1/3 justify-center relative">
-                                            <Image src={experience.CompanyLogo} width={"100"} height={"100"}
+                                        <div className="mb-6 lg:mb-0 justify-center relative h-24 w-24 lg:absolute">
+                                            <Image src={experience.CompanyLogo} layout={"fill"} objectFit={"cover"}
                                                    className={"border-cyan-500 border-4"}></Image>
                                         </div>
-                                        <div
-                                            className="flex flex-wrap justify-center text-center lg:text-left lg:block">
-                                            <h3 className="text-white text-3xl font-semibold">{experience.CompanyName}</h3>
-                                            <h4 className="text-gray-400 text-xl font-semibold">{experience.CompanyLocation} - {experience.CompanyCountry}</h4>
-                                            <h5 className={"text-gray-400 text-xl font-semibold"}>{new Date(experience.StartDate).toLocaleDateString(locale)} - {new Date(experience.EndDate).toLocaleDateString(locale)}</h5>
+                                        <div className="flex flex-wrap justify-center text-center lg:text-left lg:block">
+                                            <h3 className="text-white text-3xl font-semibold w-full">{experience.CompanyName}</h3>
+                                            <h4 className="text-gray-400 text-xl font-semibold w-full">{experience.CompanyLocation} - {experience.CompanyCountry}</h4>
+                                            <h4 className={"text-gray-400 text-xl font-semibold w-full"}>{new Date(experience.StartDate).toLocaleDateString(locale)} - {new Date(experience.EndDate).toLocaleDateString(locale)}</h4>
+                                            <h4 className={"text-gray-400 text-xl font-semibold w-full"}>{experience.Experience_ContractType[0].contracttypes.label}</h4>
                                             <div
                                                 className="w-full lg:w-auto flex flex-wrap justify-center lg:justify-start gap-3 mt-6 mb-8">
                                                 {experience.Experiences_Labels.map(expLabel => (
